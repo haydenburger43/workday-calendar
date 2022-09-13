78cc76c0dcbacb2bfbede68366d3fc5eb46fa845
hayden <haydenburger43@gmail.com>
2022-09-12 20:25:03 -0500

    added script.js

--- Develop/app.js
+++ Develop/app.js
@@ -1,3 +1,20 @@
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+
 var timeDisplayEl = document.querySelector("#time-display");
 
 timeDisplayEl.textContent = moment();
+
+
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+
+        localStorage.setItem(time, text);
+    })
+   
+    function timeTracker() {
+        var timeNow = moment().hour();
+
+
+    $("rows").val(localStorage.getItem());
+
